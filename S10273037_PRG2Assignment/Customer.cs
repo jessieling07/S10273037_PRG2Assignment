f80ebcc080e39c6be6e9b5e878ad0521f6ef562f
jessieling07 <jessie8887ling@gmail.com>
2026-01-21 17:06:36 +0800

    I added private string for customer name

---
+++ S10273037_PRG2Assignment/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10273037_PRG2Assignment
+{
+    internal class Customer
+    {
+        
+        private string customerName;
+        public string CustomerName { get; set; }
+    }
+}
