c6df41972a748a9e46c7c038b646249513cadc6d
Lu Sijin <Sijin5u>
2026-01-21 16:50:11 +0800

    try to push

---
+++ S10273037_PRG2Assignment/Restaurant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10273037_PRG2Assignment
+{
+    internal class Restaurant
+    {
+        private string restaurantId;
+        public string RestaurantId { get; set; }
+    }
+}
