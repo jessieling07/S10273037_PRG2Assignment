59ab90524d3d5ecb2a42f0626f5bb7d705668b73
Lu Sijin <Sijin5u>
2026-01-21 17:19:20 +0800

    add FoodItem class

---
+++ S10273037_PRG2Assignment/FoodItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10273037_PRG2Assignment
+{
+    internal class FoodItem
+    {
+    }
+}
