48dc629984c6e8c9aa16240ea95803a7ca545301
Lu Sijin <Sijin5u>
2026-01-21 18:10:33 +0800

    restaurant class

--- S10273037_PRG2Assignment/Restaurant.cs
+++ S10273037_PRG2Assignment/Restaurant.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 //==========================================================
 // Student Number : S10274083K
 // Student Name : Lu Sijin
@@ -19,5 +20,19 @@ namespace S10273037_PRG2Assignment
         public string RestaurantId { get; set; }
         public string RestaurantName {  get; set; }
         public string RestaurantEmail { get; set; }
+        public List<FoodItem> Menu { get; set; }
+        public Queue<Order> OrderQueue { get; set; }
+        public List<SpecialOffer> SpecialOffers { get; set; }
+
+        public Restaurant(string id, string name, string email)
+        {
+            RestaurantId = id;
+            RestaurantName = name;
+            RestaurantEmail = email;
+            Menu = new List<FoodItem>();
+            OrderQueue = new Queue<Order>();
+            SpecialOffers = new List<SpecialOffer>();
+        }
+       
     }
 }
